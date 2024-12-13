d093c17114f0fc968d6e1d53b89c457a463d945e
jcoade12 <117692495+jcoade12@users.noreply.github.com>
2024-12-13 02:36:16 -0330

    Delete src directory

--- src/main/java/com/golfclub/repositories/TournamentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.golfclub.repositories;
-
-import com.golfclub.models.Tournament;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TournamentRepository extends JpaRepository<Tournament, Long> {
-}
